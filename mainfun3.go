3c63cd88e57c2992bf95704803ebd6864e152598
RAMACHANDIRAN G <ramachandiran2231@gmail.com>
2025-06-25 10:09:39 +0530

    some error cleared

--- mainfun3.go
+++ mainfun3.go
@@ -3,22 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	// Type Conversion
+	
 	var a int64 = 4
-	var b int = int(a)  // Explicit Type Conversion
+	var b int = int(a) 
 
 	var c float64 = 6.5
 
-	// Explicit Type Conversion
-	var result = float64(b) + c  // Works
+	
+	var result = float64(b) + c  
 
 	fmt.Println(result)
 
-
-	// ==================
-
-
-	// The general syntax for converting a value v to a type T is T(v)
 	var myInt int = 65
 	var myUint uint = uint(myInt)
 	var myFloat float64 = float64(myInt)
