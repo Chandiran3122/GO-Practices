7ef8240a12264f416a18cf8d25f3732b5ee6041d
RAMACHANDIRAN G <ramachandiran2231@gmail.com>
2025-06-25 10:10:06 +0530

    fix : changed grammatical error

--- mainfun4.go
+++ mainfun4.go
@@ -4,14 +4,11 @@ import "fmt"
 
 func main() {
 	var myBoolean bool = true
-	var anotherBoolean = false // Inferred type
+	var anotherBoolean = false 
 
 	var truth = 3 <= 5
 	var falsehood = 10 != 10
-
-	// Short Circuiting
-	var res1 = 10 > 20 && 5 == 5     // Second operand is not evaluated since first evaluates to false
-	var res2 = 2*2 == 4 || 10%3 == 0 // Second operand is not evaluated since first evaluates to true
-
+	var res1 = 10 > 20 && 5 == 5    
+	var res2 = 2*2 == 4 || 10%3 == 0 
 	fmt.Println(myBoolean, anotherBoolean, truth, falsehood, res1, res2)
 }
