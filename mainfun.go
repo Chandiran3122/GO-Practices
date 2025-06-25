fd97b4fb0ebeacd3b4c37c5aca3a28b5a128e58d
RAMACHANDIRAN G <ramachandiran2231@gmail.com>
2025-06-25 10:08:08 +0530

    some unwanted command are delete

--- mainfun.go
+++ mainfun.go
@@ -7,16 +7,16 @@ import (
 
 func main() {
 	var a, b = 4, 5
-	var res1 = (a + b) * (a + b)/2  // Arithmetic operations
+	var res1 = (a + b) * (a + b)/2  
 
-	a++ // Increment a by 1
+	a++ 
 
-	b += 10 // Increment b by 10
+	b += 10 
 
-	var res2 = a ^ b // Bitwise XOR
+	var res2 = a ^ b 
 
 	var r = 3.5
-	var res3 = math.Pi * r * r  // Operations on floating-point type
+	var res3 = math.Pi * r * r 
 
 	fmt.Printf("res1 : %v, res2 : %v, res3 : %v\n", res1, res2, res3)
 }
