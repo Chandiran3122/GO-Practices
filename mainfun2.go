4f5fb0ae71593a9b84426717694bcebbe9f847f6
RAMACHANDIRAN G <ramachandiran2231@gmail.com>
2025-06-25 10:09:12 +0530

    some error cleared

--- mainfun2.go
+++ mainfun2.go
@@ -3,13 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	// Type inference
-	var name = "Rajeev Singh" // Type declaration is optional here.
+	var name = "Rajeev Singh" 
 	fmt.Printf("Variable 'name' is of type %T\n", name)
 
-	//================================
-
-	// Multiple variable declarations with inferred types
 	var firstName, lastName, age, salary = "John", "Maxwell", 28, 50000.0
 
 	fmt.Printf("firstName: %T, lastName: %T, age: %T, salary: %T\n",
