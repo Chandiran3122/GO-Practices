246bf35e6c557fb0d56652528940ebceccae87f0
RAMACHANDIRAN G <ramachandiran2231@gmail.com>
2025-06-25 10:04:37 +0530

    Add files via upload
    all variable changes in the main function

--- main.go
+++ main.go
@@ -1,16 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"math"
+)
 
-func avg(x float64, y float64) float64 {
-	return (x + y) / 2
+func getStockPriceChangeWithError(prevPrice, currentPrice float64) (float64, float64, error) {
+	if prevPrice == 0 {
+		err := errors.New("Previous price cannot be zero")
+		return 0, 0, err
+	}
+	change := currentPrice - prevPrice
+	percentChange := (change / prevPrice) * 100
+	return change, percentChange, nil
 }
 
 func main() {
-	p := 23.34
-	q := 36.25
+	prevStockPrice := 0.0
+	currentStockPrice := 100000.0
 
-	result := avg(p, q)
+	change, percentChange, err := getStockPriceChangeWithError(prevStockPrice, currentStockPrice)
 
-	fmt.Printf("Average of %.2f and %.2f = %.2f\n", p, q, result)
+	if err != nil {
+		fmt.Println("Sorry! There was an error: ", err)
+	} else {
+		if change < 0 {
+			fmt.Printf("The Stock Price decreased by $%.2f which is %.2f%% of the prev price\n", math.Abs(change), math.Abs(percentChange))
+		} else {
+			fmt.Printf("The Stock Price increased by $%.2f which is %.2f%% of the prev price\n", change, percentChange)
+		}
+	}
 }
--- main1.go
+++ main1.go
@@ -5,17 +5,17 @@ import (
 	"math"
 )
 
-func getNamedStockPriceChange(prevPrice, currentPrice float64) (change, percentChange float64) {
-	change = currentPrice - prevPrice
-	percentChange = (change / prevPrice) * 100
+func getStockPriceChange(prevPrice, currentPrice float64) (float64, float64) {
+	change := currentPrice - prevPrice
+	percentChange := (change / prevPrice) * 100
 	return change, percentChange
 }
 
 func main() {
-	prevStockPrice := 100000.0
-	currentStockPrice := 90000.0
+	prevStockPrice := 0.0
+	currentStockPrice := 100000.0
 
-	change, percentChange := getNamedStockPriceChange(prevStockPrice, currentStockPrice)
+	change, percentChange := getStockPriceChange(prevStockPrice, currentStockPrice)
 
 	if change < 0 {
 		fmt.Printf("The Stock Price decreased by $%.2f which is %.2f%% of the prev price\n", math.Abs(change), math.Abs(percentChange))
